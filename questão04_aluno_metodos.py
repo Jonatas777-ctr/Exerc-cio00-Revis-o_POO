f3a2678dcdd848e12750c64e270480d87e403027
Jonatas777-ctr <jonatasdavi643@gmail.com>
2026-09-18 21:46:26 -0300

    Add files via upload

---
+++ questão04_aluno_metodos.py
@@ -0,0 +1,31 @@
+class aluno:
+    def __init__(self, matricula, nome, nota1, nota2, nota3, nota4, nota5):
+        self.matricula = matricula
+        self.nome = nome
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.nota3 = nota3
+        self.nota4 = nota4
+        self.nota5 = nota5
+
+    def calcular_media(self):
+        soma = self.nota1 + self.nota2 + self.nota3 + self.nota4 + self.nota5
+        self.media = soma/5 
+        print(f"A média do aluno {self.nome} é: {self.media}.")
+
+    def verificar_aprovacao(self):
+        if self.media >= 7.0:
+            print(f"O aluno {self.nome} está aprovado.")
+        else:
+         (f"O aluno {self.nome} está reprovado.")
+         
+
+
+aluno1 = aluno("123", "Jonatas", 6, 8, 7, 10, 9)
+aluno2 = aluno("321", "Fulano", 5, 9, 2, 8, 3)
+aluno3 = aluno("765", "Ciclano", 8, 3, 4, 9, 2)  
+
+print("--------------------------------------------------------------------------------")
+aluno1.calcular_media()
+aluno1.verificar_aprovacao()
+print("--------------------------------------------------------------------------------")
